d0684a7ddc601b91143f98e0f7e46fef050145aa
ZoutOP <39684507+ZoutOP@users.noreply.github.com>
2023-03-08 13:31:00 +1300

    Update format_all_windows.bat

--- scripts/format_all_windows.bat
+++ scripts/format_all_windows.bat
@@ -9,7 +9,7 @@ echo ---------------------------------------------------------------------------
 echo   
 for /r %%x in (*.py) do (
 echo Found file to format: %%~nx.py
-py -m yapf "%%x" -i --style "{based_on_style: google, column_limit: 80, indent_width: 2}" && echo   Formatted to google style.
+py -m yapf "%%x" -i --style "{based_on_style: google, column_limit: 100, indent_width: 2}" && echo   Formatted to google style.
 py -m unify -i "%%x" >nul 2>&1 && echo   Unified all strings to: '
 py -m isort "%%x" && echo   Sorted all python imports.
 set /a modified+=1
